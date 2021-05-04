842af18e93c7d628abe83b0c036e3df9cb8c2fcd
Atharv-Coding <80381623+Atharv-Coding@users.noreply.github.com>
2021-05-04 13:09:22 +0530

    Add files via upload

---
+++ Console Web App.js
@@ -0,0 +1,15 @@
+var string1 = 'Atharv';
+var string2 = ' Pethkar';
+var concat = string1.concat(string2);
+console.log(concat);
+
+var num1 = 75;
+var num2 = 25;
+var sum = num1 + num2;
+var sub = num1 - num2;
+var multipy = num1 * num2;
+var divide = num1 / num2;
+console.log("The Sum of 75 + 25 is: " + sum);
+console.log("The Subraction of 75 - 25 is: " + sub);
+console.log("The Multiplication of 75 x 25 is: " + multipy);
+console.log("The Divide of 75 / 25 is: " + divide);
